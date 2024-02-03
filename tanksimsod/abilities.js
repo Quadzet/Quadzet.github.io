ac2a80d4748e882baab1a1318301dc185f0593ca
JoakimWellmar <joakim.vellmar@gmail.com>
2024-02-03 23:04:53 +0100

    Dev SoD: Devastate now scales with haste.

--- tanksimsod/abilities.js
+++ tanksimsod/abilities.js
@@ -581,7 +581,7 @@ class Devastate extends Ability {
       })
       if (target.stats.bonuses.armorDebuff) // IEA or Homunculi
         stacks = 5;
-      let damage = ((source.stats.MHMin + Math.random()*(source.stats.MHMax - source.stats.MHMin)*1000)/source.stats.MHSwing + source.getAP()/14) * (1 + stacks * 0.1) + target.additivePhysBonus;
+      let damage = ((source.stats.MHMin + Math.random()*(source.stats.MHMax - source.stats.MHMin))*1000/source.stats.MHSwing + source.getAP()/14) * (1 + source.hastePerc/100) * (1 + stacks * 0.1) + target.additivePhysBonus;
       damage *= (1 - armorReduction(source.stats.level, target.getArmor())) * source.getDamageMod()
       let damageEvent = rollAttack(source, target, damage, true, false, false, true)
       damageEvent.trigger = true;
